bd059e4952b13d74e8c221d54413df7558743ffc
João S. O. Bueno <gwidion@gmail.com>
2015-06-18 00:19:48 -0300

    Retrieved shell_rip example from PyconBrasil 2008

---
+++ shell_rip.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+
+class ExecImediate(object):
+    def __getattr__(self, command):
+        return os.system(command)
+
+s = ExecImediate()
+
+
+class ExecDelayed(object):
+    def __getattr__(self, command):
+        def call(*args):
+            return os.popen("%s %s" % 
+                                (command,
+                                 " ".join(args)
+                                )
+                            ).read()
+        return call
+sh = ExecDelayed()
